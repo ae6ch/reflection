6d60a08d76b53f65678bfdf28a82abbc5fc90abb
Steve Rubin <ser@tch.org>
2023-07-11 10:24:41 -0700

    scene switches properly login->resetpw->login

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -2,7 +2,12 @@ package application;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * javafx controller for for login.fxml
@@ -10,16 +15,32 @@ import javafx.scene.control.TextField;
  * @author Steve Rubin
  */
 public class LoginController {
+   //private Stage stage;
+   private Scene scene;
+   private Parent root;
    @FXML
    TextField passwordField;
 
-   public void loginButtonPressed(Event e)  {
+   public void loginButtonPressed(Event e) {
       System.out.printf("Login button pressed, with Password of %s\n", passwordField.getText());
       if (passwordField.getText().equals("p")) { // Hard Coded check for now to see if the password is the default
          System.out.println("Entered the default password, switch to resetpw");
-      /*
-       * Need to switch to the resetpw.fxml file here?
-       */
+
+         try {
+                  System.out.printf("Switching resetpw\n");;
+
+            Parent root = FXMLLoader.load(getClass().getResource("resetpw.fxml"));
+            Scene scene = new Scene(root, 640, 480);
+            
+         Stage stage=(Stage)((Node)e.getSource()).getScene().getWindow();
+         stage.setScene(scene);
+        
+
+
+         } catch (Exception ex) {
+
+         }
+
       }
    }
 
--- src/application/ResetPasswordController.java
+++ src/application/ResetPasswordController.java
@@ -1,5 +1,10 @@
 package application;
 import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
 
@@ -12,6 +17,22 @@ public class ResetPasswordController {
 
    public void changePasswordButtonPressed(Event e) {
       System.out.println("Change Password button pressed");
+
+ try {
+                  System.out.printf("Switching resetpw\n");;
+
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(root, 640, 480);
+            
+         Stage stage=(Stage)((Node)e.getSource()).getScene().getWindow();
+         stage.setScene(scene);
+        
+
+
+         } catch (Exception ex) {
+
+         }
+      
 
    }
 }
