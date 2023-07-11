a29e7a58798ef10a81ece1c65f4c0b8c871ae658
Steve Rubin <ser@tch.org>
2023-07-11 10:29:04 -0700

    fixed indentation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,42 +1,29 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
-	private static Stage stage;
-
-	public static Stage getStage() {
-		return stage;
-	}
-
 
 	@Override
 	public void start(Stage stage) {
-
 		try {
-       Parent loginFxml = FXMLLoader.load(getClass().getResource("login.fxml"));
+			Parent loginFxml = FXMLLoader.load(getClass().getResource("login.fxml"));
 
+			Scene loginScene = new Scene(loginFxml, 640, 480);
 
+			stage.setTitle("Reflection");
+			stage.setScene(loginScene);
+			stage.show();
 
-        Scene loginScene = new Scene(loginFxml, 640, 480);
-
-    
-        stage.setTitle("Reflection");
-        stage.setScene(loginScene);
-        stage.show();
-
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
