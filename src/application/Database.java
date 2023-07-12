47761b6693224659c72e2edb336c1e1ac641dbe1
Steve Rubin <ser@tch.org>
2023-07-12 13:36:15 -0700

    added JournalEntryController to save journal entries to database

--- src/application/Database.java
+++ src/application/Database.java
@@ -4,13 +4,15 @@ import java.sql.*;
 
 public class Database {
    Connection db = null;
+
    public Database() {
       db = databaseOpen("reflection.db");
    }
-   
+
    public Connection getConnection() {
       return db;
    }
+
    private Connection databaseOpen(String dbFile) {
       Connection c = null;
       try {
@@ -24,7 +26,8 @@ public class Database {
 
       // config table is a key value store. It is used to store our settings (user
       // password, etc). After creating the config table, add a entry called password
-      // with the value of "p", and another entry called "firstlaunch" with a value of 1.
+      // with the value of "p", and another entry called "firstlaunch" with a value of
+      // 1.
       // This will be used to check if the user has launched the program before.
       try {
          Statement stmt = c.createStatement();
@@ -34,6 +37,9 @@ public class Database {
          stmt.executeUpdate(sql);
          sql = "INSERT OR IGNORE INTO config (key, value) VALUES ('firstlaunch', '1');";
          stmt.executeUpdate(sql);
+         sql = "CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY, title TEXT, content TEXT, date INTEGER);";
+         stmt.executeUpdate(sql);
+
          stmt.close();
       } catch (Exception e) {
          System.err.println(e.getClass().getName() + ": " + e.getMessage());
--- src/application/JournalEntryController.java
+++ src/application/JournalEntryController.java
@@ -1,13 +1,25 @@
 package application;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTimePicker;
 
 import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Control;
+import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
 import javafx.stage.Stage;
 
 /**
@@ -16,20 +28,64 @@ import javafx.stage.Stage;
  * @author Steve Rubin
  */
 public class JournalEntryController {
+   @FXML
+   private TextField titleField;
+   @FXML
+   private HTMLEditor bodyField;
+   @FXML
+   private JFXDatePicker dateStamp;
+   @FXML
+   private JFXTimePicker timeStamp;
 
-   /** 
+   Database db;
+
+   public JournalEntryController() {
+      System.out.println("JournalEntryController constructor called");
+      db = new Database();
+   }
+
+   /**
     * Event handler for any buttons
     * 
     * @param e event
     */
- public void buttonPressed(Event e) {
+   public void buttonPressed(Event e) {
       switch (((Control) e.getSource()).getId()) {
          case "save": // Save (with fall thru to cancel so it returns to mainmenu?)
             // Do the stuff here to save
+            // Get the values from the fields
+            String title = titleField.getText();
+            String body = bodyField.getHtmlText();
+            LocalDate date = dateStamp.getValue();
+            LocalTime time = timeStamp.getValue();
+            LocalDateTime localDateTime = LocalDateTime.of(date, time);
+            System.out.println("Title: " + title);
+            System.out.println("Body: " + body);
+            long timeInSeconds = localDateTime.toEpochSecond(ZoneOffset.UTC);
+
+            System.out.println("DateTime: " + timeInSeconds);
+
+            // Save the values to the database
+            try {
+               PreparedStatement pstmt = null;
+               pstmt = db.getConnection()
+                     .prepareStatement("INSERT INTO entries (title, content, date) VALUES (?, ?, ?)");
+
+               pstmt.setString(1, title);
+               pstmt.setString(2, body);
+               pstmt.setLong(3, timeInSeconds);
+               pstmt.executeUpdate();
+               pstmt.close();
+
+            } catch (SQLException sqle) {
+               System.out.println("SQLException: " + sqle.getMessage());
+            } finally {
+            }
+
          case "cancel": // Change Password Button
             changeScene(e, "mainmenu.fxml");
             break;
-         
+
          default:
             System.out.printf("unknown event: %s\n", ((Control) e.getSource()).getId());
             break;
@@ -37,13 +93,13 @@ public class JournalEntryController {
       }
    }
 
-
    /**
     * Change the scene
-    * @param e Event where we can get the stage from
+    * 
+    * @param e    Event where we can get the stage from
     * @param fxml fxml file of new scene
     */
-      private void changeScene(Event e, String fxml) {
+   private void changeScene(Event e, String fxml) {
       try {
          Parent root = FXMLLoader.load(getClass().getResource(fxml));
          Scene scene = new Scene(root, 640, 480);
